febe557ce73f818764b119ea67c52d883e0d0b66
Otabek <mavlonovo@tamu.edu>
2023-08-07 17:52:45 -0500

    added handlers and password checker

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (req.body["password"] == "one") {
+    res.send("ILoveProgramming");
+  } else {
+    res.send("incorrect");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
